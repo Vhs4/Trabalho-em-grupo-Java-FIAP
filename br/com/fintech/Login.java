763b640129672664eea88542d5537a9571a40ecc
joaomacosdev <joamarcosdev@gmail.com>
2024-08-19 13:40:06 -0300

    Classe Conta finalizada construção da nova classe Gerenciamento e ajustes na classe Login

--- br/com/fintech/Login.java
+++ br/com/fintech/Login.java
@@ -3,9 +3,11 @@ package br.com.fintech;
 import java.util.Scanner;
 
 public class Login {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void realizarLogin() {
         System.out.print("Nome de usuário: ");
         String username = scanner.nextLine();
 
@@ -18,7 +20,23 @@ public class Login {
         } else {
             System.out.println("Nome de usuário ou senha incorretos.");
         }
-
-        scanner.close();
     }
+
+    public void verificarLogin() {
+        String currentUser = UserSession.getUsername();
+
+        if (currentUser == null) {
+            System.out.println("Nenhum usuário está logado. Iniciando o processo de login...");
+            realizarLogin();
+            currentUser = UserSession.getUsername();
+        }
+
+        if (currentUser != null) {
+            System.out.println("Usuário logado: " + currentUser);
+        } else {
+            System.out.println("Login falhou. Encerrando o programa.");
+            System.exit(0);  // Encerra o programa se o login falhar
+        }
+    }
+
 }
---
+++ out/production/Trabalho-em-grupo-Java-FIAP/README.md
@@ -0,0 +1,2 @@
+# Trabalho-em-grupo-Java-FIAP
+Trabalho em grupo da minha turma de análise e desenvolvimento de sistemas na FIAP (Faculdade de Informática e Administração Paulista)
