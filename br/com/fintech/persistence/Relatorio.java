15210025b1adf5267c97ffa3e34adb61cfafbe44
Vhs4 <vhszera44444@gmail.com>
2024-08-25 11:17:55 -0300

    FEAT: Trabalho finalizado

--- br/com/fintech/persistence/Relatorio.java
+++ br/com/fintech/persistence/Relatorio.java
@@ -10,18 +10,22 @@ public class Relatorio {
     private LocalDate dataGeracao;
     private TipoRelatorio tipo;
 
+    // Construtor padrão
     public Relatorio() {}
 
+    // Construtor com parâmetros
     public Relatorio(Long idRelatorio, TipoRelatorio tipo) {
         this.idRelatorio = idRelatorio;
         this.dataGeracao = LocalDate.now();
         this.tipo = tipo;
     }
 
+    // Método para gerar o relatório
     public void gerarRelatorio() {
         System.out.println("Gerando relatório do tipo: " + tipo + " em " + dataGeracao);
     }
 
+    // Método para imprimir detalhes do relatório
     public void imprimirDetalhes() {
         System.out.println("Relatório ID: " + idRelatorio + ", Tipo: " + tipo + ", Data de Geração: " + dataGeracao);
     }
